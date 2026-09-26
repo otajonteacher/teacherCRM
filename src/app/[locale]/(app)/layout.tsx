01bee1a138322a12c6c3b90421b2c51151c042ce
otajonteacher <asatullayevotajon8@gmail.com>
2026-09-26 15:23:17 +0500

    fix: navbar shell user name type xatosini tuzatish

--- src/app/[locale]/(app)/layout.tsx
+++ src/app/[locale]/(app)/layout.tsx
@@ -24,7 +24,7 @@ export default async function AppLayout({
     <AppShell
       role={role}
       appName={t("appName")}
-      userName={name}
+      userName={name ?? ""}
       roleLabel={tr(role)}
       mobileNav={<MobileNav role={role} />}
       languageSwitcher={<LanguageSwitcher />}
